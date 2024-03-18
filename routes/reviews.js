a0105cefb0146ca62b9790067b317caee163e68d
Grace Clower <clower.grace@gmail.com>
2024-03-18 15:56:12 -0400

    seeded data and created insertDatd function

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import * as controllers from "../controllers/reviews.js";
-import restrict from "..helpers/restrict.js";
+import restrict from "../helpers/restrict.js";
 
 const router = Router();
 
-router.get("/", controllers.getReviews);
-router.get("/:id", controllers.getReview);
+// router.get("/", controllers.getReviews);
+// router.get("/:id", controllers.getReview);
 router.post("/", restrict, controllers.createReview);
-router.put("/:id", restrict, controllers.updateReview);
-router.delete("/:id", restrict, controllers.deleteReview);
+// router.put("/:id", restrict, controllers.updateReview);
+// router.delete("/:id", restrict, controllers.deleteReview);
 
 export default router;
--- seed/data.js
+++ seed/data.js
@@ -1,11 +1,13 @@
 import db from '../db/connection.js';
-import bcrypt from 'bcrypt';
-import fetch from "node-fetch";
-import { promises as fsPromises} from "fs";
+import chalk from 'chalk';
+import gamesData from "./games.json" assert {type: "json"}
 import Game from "../models/Game.js"
 
 const insertData = async () => {
     await db.dropDatabase();
-    
+    await Game.create(gamesData)
+    console.log(chalk.magentaBright("Games data added! NOICE"))
+    await db.close()
 }
 
+insertData();
